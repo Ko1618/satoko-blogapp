39c8c41c746e2cd1a2bd718068a17e3890bfc81c
佐藤　航 <>
2021-02-04 19:56:43 +0900

    ajaxによるライク、コメントの表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ john = User.create!(email: 'john@example.com', password: 'password')
 emily = User.create!(email: 'emily@example.com', password: 'password')
 
 5.times do
-    john.articles.create(
+    john.articles.create!(
         title: Faker::Lorem.sentence(word_count: 5),
         content: Faker::Lorem.sentence(word_count: 100)
     )
 end
 
 5.times do
-    emily.articles.create(
+    emily.articles.create!(
         title: Faker::Lorem.sentence(word_count: 5),
         content: Faker::Lorem.sentence(word_count: 100)
     )
