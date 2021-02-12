8f2ca19f1dcfcb82675663cee972a13ab3df59d0
佐藤　航 <>
2021-02-13 00:18:51 +0900

    system_spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   let!(:user) { create(:user) }
-  let!(:article) { create_list(:article, 3, user: user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
 
   describe 'GET /articles' do
     it '200ステータスが返ってくる' do
